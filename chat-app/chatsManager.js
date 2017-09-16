f2b6b5e11473c178e73462a36a4aa00284f1dbc4
Akshay Deshmukh <akshay.deshmukh@nitorinfotech.com>
2017-09-16 14:37:32 +0530

    mongorepository file is removed

--- chat-app/chatsManager.js
+++ chat-app/chatsManager.js
@@ -1,4 +1,15 @@
-const mongoRepository = require('./mongoRepository.js');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://coder2xx:<password>@cluster4-shard-00-00-i2q0s.mongodb.net:27017,cluster4-shard-00-01-i2q0s.mongodb.net:27017,cluster4-shard-00-02-i2q0s.mongodb.net:27017/Akshay?ssl=true&replicaSet=Cluster4-shard-0&authSource=admin");
+
+const db = mongoose.connection;
+db.on('error', () => {
+    console.error('Connection error for MONGODB...');
+});
+db.once('open', () => {
+    console.log("Connected to MONGODB successfully...");
+});
+
+const schema = mongoose.Schema;
 
 const chatSchema = {
     from: String,
@@ -6,7 +17,7 @@ const chatSchema = {
     text: String
 };
 
-const chatModel = mongoRepository.getModel('Chat', chatSchema);
+const chatModel = mongoose.model('Chat', new schema(chatSchema));
 
 const save_chat = function (chat) {
     console.log(chat);
@@ -15,7 +26,11 @@ const save_chat = function (chat) {
         clientId: chat.clientId,
         text: chat.text
     });
-    mongoRepository.create(data);
+    data.save(function (err, fluffy) {
+        if (err) {
+            return console.error(err);
+        }
+    });
 };
 
 const get_history = function (callback) {
--- chat-app/mongoRepository.js
+++ chat-app/mongoRepository.js
@@ -1,54 +1,5 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://coder2xx:<password>@cluster4-shard-00-00-i2q0s.mongodb.net:27017,cluster4-shard-00-01-i2q0s.mongodb.net:27017,cluster4-shard-00-02-i2q0s.mongodb.net:27017/Akshay?ssl=true&replicaSet=Cluster4-shard-0&authSource=admin");
-
-const db = mongoose.connection;
-db.on('error', () => {
-    console.error('Connection error for MONGODB...');
-});
-db.once('open', () => {
-    console.log("Connected to MONGODB successfully...");
-});
-
-const schema = mongoose.Schema;
-
-const mGetModel = (modelTitle, modelInfo) => {
-    const modelSchema = new schema(modelInfo);
-    return mongoose.model(modelTitle, modelSchema);
-};
-
-const mCreate = function (document) {
-    document.save(function (err, fluffy) {
-        if (err) {
-            return console.error(err);
-        }
-    });
-};
-
-const mUpdate = function () {
-    var history = new chatHistory({ from: fromUser, message: chatMessage });
-    history.save(function (err, fluffy) {
-        if (err) return console.error(err);
-    });
-};
-
-const mDelete = function () {
-    var history = new chatHistory({ from: fromUser, message: chatMessage });
-    history.save(function (err, fluffy) {
-        if (err) return console.error(err);
-    });
-};
-
-const mRead = function () {
-    var history = new chatHistory({ from: fromUser, message: chatMessage });
-    history.save(function (err, fluffy) {
-        if (err) return console.error(err);
-    });
-};
 
 module.exports = {
     getModel: mGetModel,
     create: mCreate,
-    read: mRead,
-    update: mUpdate,
-    delete: mDelete
 };
